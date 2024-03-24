b44b3e54c71a52b11daede565e8006a4d0978697
Arimlna <160694744+Arimlna@users.noreply.github.com>
2024-03-24 14:23:02 +0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,11 @@
+saldo = 100000
+rokok = 20000
+minyak = 25000
+gula = 11000
+diskon = 10000
+total_belanjaan = rokok+minyak+gula
+keseluruhan_belanjaan =saldo-rokok-minyak-gula+diskon               # ambil nilai saaldonya dlu baru dikurangin total
+print("total uang ucup = ",saldo )
+print("total belanjaan dan kembalian =", keseluruhan_belanjaan)
+print('============================================================')
+                    
